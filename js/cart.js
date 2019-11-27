8082d329c000d7de31599042fa7d7e6ed0acc23d
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-11-27 21:07:13 +0200

    lab14

--- js/cart.js
+++ js/cart.js
@@ -19,25 +19,48 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  var tableDelet= document.getElementById('cart');
+  tableDelet.deletRow(table.row.length-1);
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
+  for (i = 0; i < cart.length; i++) {
+      var th = document.createElement('TH')
+      th.appendChild(document.createTextNode(cart[i]));
+      tr.appendChild(th);
   // TODO: Find the table body
-
+var tableBody= document.getElementById('cart');
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
+  var tr = document.createElement('TR');
+  tableBody.appendChild(tr);
+
   // TODO: Create a TD for the delete link, quantity,  and the item
+  var tdDelete= document.createElement('TD');
+
+  tr.appendChild(tdDelete);
+  vat tdQuantity= document.createElement('TD')
+  tr.appendChild(tdQuantity);
+  var tdItem= document.createElement('TD');
+  tr.appendChild(tdItem);
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
 function removeItemFromCart(event) {
+  // table.removeEventListener('click', removeItemFromCart);
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  cart.removeItem(cartItems);
   // TODO: Save the cart back to local storage
+  var cartString=JSON.stringify(this.items);
+  localStorage.setItem('backToLocal',cartString);
   // TODO: Re-draw the cart table
+////no need to repeat every thing , just call the function /////
+showCart();
 
 }
 
